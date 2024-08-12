3dff27bfaa851ce3c27b3372973fafa0778a2130
wwoah <phillipkbishop@gmail.com>
2024-08-12 16:20:16 -0700

    Completed Exercise 04 Remove From Array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr) {
+
+    const args = Array.from(arguments);
+    args.splice(0,1);
+
+    const inputLen = inputArr.length;
+    const argLen = args.length;
+
+    for(let i = 0; i < inputLen; i++){
+        for(let j = 0; j < argLen; j++) {
+            if(inputArr[i] === args[j]){
+                inputArr.splice(i,1);
+                i = 0;
+                j = 0;
+            }
+        }
+    }
+
+    return inputArr;
 };
 
 // Do not edit below this line
