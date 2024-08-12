be86721d9e371197aac474d2c8465ab9772ad2ea
wwoah <phillipkbishop@gmail.com>
2024-08-12 12:55:46 -0700

    Completed exercise 02 Repeat Strings.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, iterations) {
 
+    if(iterations < 0) { return "ERROR";}
+
+    let result = "";
+
+    for(let i = 0; i < iterations; i++){
+        let tempResult = result + str;
+        result = tempResult;
+    }
+    return result;
 };
 
 // Do not edit below this line
