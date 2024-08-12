f76211ac9a7f4d17cf7829348b6e27ec146bf3cb
wwoah <phillipkbishop@gmail.com>
2024-08-12 16:53:02 -0700

    Completed Exercise 05 Sum All.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(left, right) {
+    
+    if(left < 0 || right < 0 ) { return "ERROR"; }
+    if(typeof(left) != "number" || typeof(right) != "number") { return "ERROR"; }
+    if(!Number.isInteger(left) || !Number.isInteger(right)) { return "ERROR"; }
+
+    let upperbound = Math.max(left,right);
+    let sum = 0;
+
+    for(let i = 0; i <= upperbound; i++){ sum += i; }
+
+    return sum;
 
 };
 
