db78efa4827ce3f976caafb369c51f6daa03b696
wwoah <phillipkbishop@gmail.com>
2024-08-12 17:54:11 -0700

    Completed Exercise 07 Temperature Conversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {  
+  let CtoF = ((F-32.00)*5.00)/9.00;
+  let result = Math.round(CtoF * 10) / 10
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let FtoC = (9.00/5.00)*C + 32.00;
+  let result = Math.round(FtoC * 10) / 10
+  return result;
 };
 
 // Do not edit below this line
