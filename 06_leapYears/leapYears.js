bc5c12e217c94352cdde2d0d4a8fe8cec73d2626
wwoah <phillipkbishop@gmail.com>
2024-08-12 17:37:59 -0700

    Completed Exercise 06 Leap Years.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let div4 = year % 4;
+    let div100 = year % 100;
+    let div400 = year % 400;
+
+    console.log(div4);
+    console.log(div100);
+    console.log(div400);
+
+
+    if(div100 == 0) {
+        if(div400 == 0) { return true; }
+        return false;
+    }
+    else if(year % 4 == 0) { return true; }
+    return false;
 
 };
 
