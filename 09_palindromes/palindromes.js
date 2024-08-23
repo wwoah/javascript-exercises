231f9a121a97d8e59baf4456e8b90c3d221470c5
wwoah <phillipkbishop@gmail.com>
2024-08-23 13:27:48 -0700

    Completed Exercise 09 Palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
