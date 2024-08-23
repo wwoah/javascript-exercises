3b580de524946ea7cb754ef34f4af3a07662aacf
wwoah <phillipkbishop@gmail.com>
2024-08-23 14:23:40 -0700

    Completed Exercise 12 Find The Oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    return people
+    .reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        
+        return personAge > oldestAge ? person : oldest;
+    });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
