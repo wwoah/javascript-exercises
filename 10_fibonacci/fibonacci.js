00e1b137785c223fc2eb38bb1349ab638f45de0c
wwoah <phillipkbishop@gmail.com>
2024-08-23 13:37:17 -0700

    Completed Exercise 10 Fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(+a < 0) {
+        return 'OOPS';
+    }
+    if(+a <= 1){
+        return +a;
+    }
+    return fibonacci(+a-1) + fibonacci(+a-2); 
 };
 
 // Do not edit below this line
